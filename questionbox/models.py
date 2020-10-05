5a2f5b5000db6ecf4dcdb2a7d46f8bc0e62cfb8c
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-10-05 17:59:59 -0400

    Question and Answer models first version, not migrated

--- questionbox/models.py
+++ questionbox/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from users.models import User
+from datetime import timedelta
 
 # Create your models here.
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    body = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def niceCreated(self):
+        nice_created = self.created_at - timedelta(hours=4)
+        return nice_created.strftime("Created on %A at %I:%M %p")
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Answer(models.Model):
+    body = models.TextField(null=False, blank=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def niceCreated(self):
+        nice_created = self.created_at - timedelta(hours=4)
+        return nice_created.strftime("Created on %A at %I:%M %p")
